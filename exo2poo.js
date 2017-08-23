20f9b37f71f71a9f8fdd149c039867fc15476fd8
cyrilforrez <cyilforrez@gmail.com>
2017-08-23 10:57:07 +0200

    create constructeur,

--- exo2poo.js
+++ exo2poo.js
@@ -1,9 +1,23 @@
-function Homme (nom, age) {
+function Personnage(nom, attack, defense, sante) {
 	this.nom = nom;
 	this.attack = attack;
-	this.defense = defense
-	this.sante = sante
-	this.getName = function(){
-		alert(this.nom);
-	};
+	this.defense = defense;
+	this.sante = sante;
+    this.combattre = function(cible){
+    	cible.sante = cible.sante - this.attack;
+    	if (cible.sante < 50) {
+    		console.log(this.nom + " " + "à infligé" + " "+ this.attack + " " + "points de degats à" + " " + cible.nom);
+    		console.log("il reste" + " " + cible.sante + " " + "de vie" + " " + "à" + " " + cible.nom);
+    	}
+};
+
 }
+
+
+
+var Personnage1 = new Personnage ("martin", 20, 10, 50);
+var Personnage2 = new Personnage ("Dupont", 10, 5, 30);
+
+
+Personnage1.combattre(Personnage2);
+Personnage2.combattre(Personnage1);
